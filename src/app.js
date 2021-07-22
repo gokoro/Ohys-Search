fb2f6c2b8c92fa78b1a9efe63ed3fd1b9cf90dbc
Gokoro <lincroe@gmail.com>
2021-07-23 01:48:26 +0900

    Add specify ip address listening for Docker

--- src/app.js
+++ src/app.js
@@ -4,10 +4,10 @@ const api = require('./api')
 
 const start = async () => {
   api()
-  loader()
+  await loader()
 
   try {
-    await fastify.listen(3000)
+    await fastify.listen(3000, '0.0.0.0')
   } catch (err) {
     fastify.log.error(err)
     process.exit(1)
