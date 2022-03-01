f5d72a3c0d5ba05af59c3b7a56cbe703594c9740
Gokoro <lincroe@gmail.com>
2022-03-01 20:45:15 +0900

    Downgrade to yarn classic

--- scripts/esbuild-plugins.cjs
+++ scripts/esbuild-plugins.cjs
@@ -1,5 +1,3 @@
 const { nodeExternalsPlugin } = require('esbuild-node-externals')
-const { pnpPlugin } = require('@yarnpkg/esbuild-plugin-pnp')
-const { commonjs } = require('@hyrious/esbuild-plugin-commonjs')
 
-module.exports = [nodeExternalsPlugin(), pnpPlugin(), commonjs()]
+module.exports = [nodeExternalsPlugin()]
