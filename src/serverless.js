403b56885864b286d2d729d0fba01a3e0b1c42c9
Gokoro <lincroe@gmail.com>
2022-03-01 21:43:21 +0900

    Aws lambda (#2)
    * Prettify fastify
    
    * Add serverless and aws-lambda-fastify
    
    * Configure serverless
    
    * Configure esbuild
    
    * Setup webpack
    
    * Downgrade to yarn classic
    
    * Add workflow for deploying

---
+++ src/serverless.js
@@ -0,0 +1,18 @@
+import awsLambdaFastify from 'aws-lambda-fastify'
+import * as Instance from './loaders/fastify.js'
+import * as Mongoose from './loaders/mongoose.js'
+
+const app = Instance.get()
+const proxy = awsLambdaFastify(app)
+
+let connection = null
+
+export const handler = async (event, context) => {
+  if (connection === null) {
+    connection = await Mongoose.connect()
+  }
+
+  await connection
+
+  return proxy(event, context)
+}
